5c31fb44846206a5c0db60f8e37fa96a0ba262d2
fantasticlife <michael@smethur.st>
2025-07-07 17:54:31 +0100

    determined date for answer

--- lib/get_questions.rb
+++ lib/get_questions.rb
@@ -35,24 +35,41 @@ module GetQuestions
       # ... we traverse the JSON to the value.
       question_value = question['value']
       
-      # We create a new question object ...
-      question = Question.new
-      question.id = question_value['id']
-      question.house = question_value['house']
-      question.tabled_on = question_value['dateTabled'].to_date
-      question.date_for_answer = question_value['dateForAnswer'].to_date
-      question.uin = question_value['uin']
-      question.answering_body_id = question_value['answeringBodyId']
-      question.answering_body_name = question_value['answeringBodyName']
-      question.is_named_day = question_value['isNamedDay']
-      question.heading = question_value['heading']
-      question.text = question_value['questionText']
-      question.asking_member_id = question_value['askingMember']['id']
-      question.asking_member_name = question_value['askingMember']['name']
-      question.asking_member_sort_name = question_value['askingMember']['listAs']
+      # We get the information we need to determine the date for answer.
+      house = question_value['house']
+      tabled_on = question_value['dateTabled'].to_date
+      published_date_for_answer = question_value['dateForAnswer'].to_date
+      is_named_day = question_value['isNamedDay']
       
-      # ... and add it to the array of questions.
-      questions << question
+      # We determine the date for answer.
+      determined_date_for_answer = determine_date_for_answer( house, tabled_on, published_date_for_answer, is_named_day )
+      
+      # We know that the determine date for answer method returns a nil value for 'ordinary' questions tabled less than eight days ago in the House of Commons.
+      # If we have determined a date for answer ...
+      if determined_date_for_answer
+      
+        # ... we create a new question object ...
+        question = Question.new
+        question.id = question_value['id']
+        question.house = house
+        question.tabled_on = tabled_on
+        question.uin = question_value['uin']
+        question.answering_body_id = question_value['answeringBodyId']
+        question.answering_body_name = question_value['answeringBodyName']
+        question.is_named_day = is_named_day
+        question.heading = question_value['heading']
+        question.text = question_value['questionText']
+        question.asking_member_id = question_value['askingMember']['id']
+        question.asking_member_name = question_value['askingMember']['name']
+        question.asking_member_sort_name = question_value['askingMember']['listAs']
+        
+        # ... assigning it the date for answer we determined ...
+        question.date_for_answer = determined_date_for_answer
+        
+      
+        # ... and add it to the array of questions.
+        questions << question
+      end
     end
     
     # We sort the questions array by date for answer.
@@ -60,5 +77,52 @@ module GetQuestions
     
     # We return the questions array.
     questions
+  end
+  
+  # A method to determine the date for answer.
+  def determine_date_for_answer( house, tabled_on, published_date_for_answer, is_named_day )
+  
+    # We create a variable to store the date for answer.
+    date_for_answer = nil
+    
+    # If the question has been tabled in the House of Lords ...
+    if house == 'Lords'
+      
+      # ... we set the date for answer to the published date for answer, knowing that date for answer is set correctly in the House of Lords.
+      date_for_answer = published_date_for_answer
+    
+    # Otherwise, if the question has been tabled in the House of Commons ...
+    elsif house = 'Commons'
+    
+      # If he question is a named day question.
+      if is_named_day
+      
+        # ... we set the date for answer to the published date for answer.
+        date_for_answer = published_date_for_answer
+        
+      # Otherwise, if the question is an 'ordinary' question ...
+      else
+      
+        # ... we base the logic on the convention that an MP can expect an ordinary day question to be answered within seven days of the question being tabled, as [outlined here](https://www.parliament.uk/about/how/business/written-answers/).
+        # We set the tabling cut-off date to eight days before today.
+        tabling_cut_off_date = Date.today - 8
+        
+        # If the tabling date is on or before the tabling cut off date ...
+        if tabled_on <= tabling_cut_off_date
+          
+          # ... we set the date for answer to the tabling date plus seven days.
+          date_for_answer = tabled_on + 7.days
+        end
+      end
+    
+    # Otherwise, if the question has been tabled in neither the House of Lords not the House of Commons ...
+    else
+    
+      # Something has gone wrong.
+      puts "Question assigned to neither House"
+    end
+    
+    # We return the date for answer.
+    date_for_answer
   end
 end
