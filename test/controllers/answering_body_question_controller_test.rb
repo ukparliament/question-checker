f2eb9354123c978fb6fab37d9143f725f4516ef6
James Jefferies <james@shedcode.co.uk>
2025-07-17 10:44:01 +0100

    Delete tests

--- test/controllers/answering_body_question_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class AnsweringBodyQuestionControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/house_answering_body_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class HouseAnsweringBodyControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
